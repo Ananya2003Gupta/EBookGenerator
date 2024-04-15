090231e57963f0c28a171726649d62283cca0cc2
Ananya2003Gupta <90386813+Ananya2003Gupta@users.noreply.github.com>
2024-04-15 19:09:39 +0530

    Added mkdocs rendering support

---
+++ example/generatedmarkdown/docs/index.md
@@ -0,0 +1,2 @@
+This is an automatically generated E-Book from PDF(s).
+Happy Reading !
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pdf2markdown import PDFProcessor
 
 def main():
     pdf_directory = os.path.join("example", "examplepdf") # Replace with the path to your PDF directory
-    markdown_directory = os.path.join("example", "generatedmarkdown")  # Replace with the path to your Markdown directory
+    markdown_directory = os.path.join("example", "generatedmarkdown", "docs")  # Replace with the path to your Markdown directory
     processor = PDFProcessor(pdf_directory, markdown_directory)
     processor.process_pdfs_in_directory()
 
