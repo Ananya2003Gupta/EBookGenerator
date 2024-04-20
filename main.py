eea9a83643f014a9a2e2c904bde9507c811138f7
Ananya Gupta <90386813+Ananya2003Gupta@users.noreply.github.com>
2024-04-21 00:39:01 +0530

    Added input function to take path from user

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from pdf2markdown import PDFProcessor
 
 def main():
-    pdf_directory = "example" # Replace with the path to your PDF directory
+    pdf_directory = input("Path of PDF Directory") # Replace with the path to your PDF directory
     markdown_directory = os.path.join("ebookgenerator", "docs")
     processor = PDFProcessor(pdf_directory, markdown_directory)
     processor.process_pdfs_in_directory()
